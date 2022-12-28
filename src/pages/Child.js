2bbb1feeae9eb739080c6b000ae8e381dacc0d23
marshallanique <marshallanique@gmail.com>
2022-12-28 16:00:50 +0600

    state

--- src/pages/Child.js
+++ src/pages/Child.js
@@ -1,23 +1,18 @@
-import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 
-const Child = () => {
-  const [count, setCount] = useState(0)
-
-
-
+const Child = ({ count, setCount }) => {
   return (
     <div style={{
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
+      paddingTop: "5%"
     }}>
       <Card style={{ width: '18rem' }}>
         <Card.Body className="text-center">
-
-          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <Button onClick={() => setCount(count + 1)}>Increment</Button>
           <h3>{count}</h3>
-          <button onClick={() => setCount(count - 1)}>Decrement</button>
+          <Button onClick={() => setCount(count - 1)}>Decrement</Button>
         </Card.Body>
       </Card>
     </div>
--- src/pages/Parent.js
+++ src/pages/Parent.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import { Card } from "react-bootstrap";
 import Child from "./Child";
 
 const Parent = () => {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      <Child />
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "5%"
+      }}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body className="text-center">
+            <h3>{count}</h3>
+          </Card.Body>
+        </Card>
+      </div>
+      <Child count={count} setCount={setCount} />
     </div>
   );
 }
