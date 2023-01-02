c915b13ce5aa2d18f3e3adca4ec2cdaadade8cf8
marshallanique <marshallanique@gmail.com>
2023-01-02 10:35:39 +0600

    add to cart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,32 @@
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const { state: { products } } = useProducts();
-  console.log(products);
+  const { state: { products, loading, error } } = useProducts();
+
+  let content;
+
+  if (loading) {
+    content = <p>loading now</p>
+  }
+
+  if (error) {
+    content = <p>error now</p>
+  }
+  if (!loading && !error && products.length === 0) {
+    content = <p>no data is showing</p>
+  }
+  if (!loading && !error && products.length) {
+    content =
+      products.map((product) => {
+        return (
+          <h3 key={product.id}>{product.username}</h3>
+        )
+      })
+
+  }
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {
-        products.map((product) => {
-          return (
-            <h3 key={product.id}>{product.username}</h3>
-          )
-        })
-      }
+      {content}
     </div>
   );
 };
--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { useProducts } from "../context/ProductProvider";
 
 const TopRated = () => {
+  const { state: { products, loading, error } } = useProducts();
+  console.log(products);
+  let content;
+
+  if (loading) {
+    content = <p>loading now</p>
+  }
+
+  if (error) {
+    content = <p>error now</p>
+  }
+  if (!loading && !error && products.length === 0) {
+    content = <p>no data is showing</p>
+  }
+  if (!loading && !error && products.length) {
+    content =
+      products.map((product) => {
+        return (
+          <h3 key={product.id}>{product.username}</h3>
+        )
+      })
+
+  }
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is featured page</h1>
+      {content}
     </div>
   );
 };
